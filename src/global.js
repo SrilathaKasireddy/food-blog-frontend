7375caad7c6ea04973721c80e4e197aa71da411a
SrilathaKasireddy <kasireddysrilatha17@gmail.com>
2022-12-26 21:26:12 +0530

    deploy

--- src/global.js
+++ src/global.js
@@ -1,4 +1,5 @@
 // export const API = "http://localhost:4000"
 
 
-export const API = "https://foodblog8.herokuapp.com"
+// export const API = "https://foodblog8.herokuapp.com"
+export const API ="https://foodbkend.onrender.com"
